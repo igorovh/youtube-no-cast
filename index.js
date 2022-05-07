865d470d4fc28200cb26f5b0d59815c138df5dbe
animekkk <animekgaming@gmail.com>
2022-05-07 21:24:18 +0200

    Finishing extension

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-document.querySelectorAll('.ytp-remote-button').forEach(button => button.remove());
+let i = 0;
+document.querySelectorAll('.ytp-remote-button').forEach(button => {
+    button.remove();
+    i++;
+});
+
+console.info(`[YT-NC] Removed ${i} buttons.`);

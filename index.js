1ff05d6349d84851064c589c73a10f614ddfcaef
animekkk <animekgaming@gmail.com>
2022-05-07 21:31:48 +0200

    Removing spaming in console

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 setInterval(() => {
     const buttons = document.querySelectorAll('.ytp-remote-button');
-    document.querySelectorAll('.ytp-remote-button').forEach(button => button.remove());
-    console.info(`[YT-NC] Removed ${buttons.length} buttons.`);
+    if(buttons.length > 0) {
+        document.querySelectorAll('.ytp-remote-button').forEach(button => button.remove());
+        console.info(`[YT-NC] Removed ${buttons.length} buttons.`);
+    }
 }, 1000)
